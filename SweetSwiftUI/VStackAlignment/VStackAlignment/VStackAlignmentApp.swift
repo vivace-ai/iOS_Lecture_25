ebd2113e6670258bbad0176712db235b5e2a7927
영감을 주고 희망을 주는 김영희 멘토 <openvision4@gmail.com>
2025-02-16 02:35:03 +0900

    VStack Alignment
    VStack Alignment

---
+++ SweetSwiftUI/VStackAlignment/VStackAlignment/VStackAlignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  VStackAlignmentApp.swift
+//  VStackAlignment
+//
+//  Created by Kim Young Hee on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct VStackAlignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
