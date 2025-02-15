2f608c4129ccbba5da92c72142ea51540ac5dfcf
영감을 주고 희망을 주는 김영희 멘토 <openvision4@gmail.com>
2025-02-16 02:10:48 +0900

    BJ_Sweet_SwiftUI_이봉원
    스윗한 SwiftUI

---
+++ SweetSwiftUI/SweetSwiftUI/SweetSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetSwiftUIApp.swift
+//  SweetSwiftUI
+//
+//  Created by Kim Young Hee on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SweetSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SweetSwiftUI/SweetSwiftUITests/SweetSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  SweetSwiftUITests.swift
+//  SweetSwiftUITests
+//
+//  Created by Kim Young Hee on 1/15/25.
+//
+
+import Testing
+@testable import SweetSwiftUI
+
+struct SweetSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
